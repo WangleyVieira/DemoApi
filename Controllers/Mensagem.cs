d1baf56cfb71528c101ce7d4c435b4f4f9ee8e31
WangleyVieira <wangleymsv10@hotmail.com>
2024-06-03 10:19:23 -0400

    Adicionar arquivos de projeto.

---
+++ Controllers/Mensagem.cs
@@ -0,0 +1,6 @@
+﻿namespace DemoApi.Controllers
+{
+    public class Mensagem
+    {
+    }
+}
---
+++ Controllers/MensagemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MensagemController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMensagem()
+        {
+            return Ok("Olá");
+        }
+    }
+}
